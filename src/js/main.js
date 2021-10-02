de50ad2886c72fb37a6d7c3699377da6ef269f00
svdevelop <verstka.sv@mail.ru>
2021-10-02 20:10:16 +0300

    firstCommit

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+const modals = document.querySelectorAll(".auth");
+
+const modal1 = document.querySelector(".auth#modal1");
+const modal2 = document.querySelector(".auth#modal2");
+const modal3 = document.querySelector(".auth#modal3");
+
+const openModal = modal => modal.classList.add("active");
+const cloesModal = modal => modal.classList.remove("active");
+
+const closeBtns = () =>
+  document
+    .querySelectorAll(".btn__close")
+    .forEach(btn =>
+      btn.addEventListener("click", e => cloesModal(e.target.parentElement))
+    );
+
+modals.forEach(modal =>
+  modal.addEventListener("click", e => {
+    if (e.target.classList.contains("btn__close")) {
+      closeBtns();
+    }
+    if (e.target.dataset.next) {
+      closeBtns();
+      
+      openModal(document.querySelector(`.auth#modal${e.target.dataset.next}`))
+    }
+  })
+);
+
+document
+  .querySelector(".btn-login")
+  .addEventListener("click", () => openModal(modal1));
+document
+  .querySelector(".main__btn")
+  .addEventListener("click", () => openModal(modal1));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneInputs = document.querySelectorAll("input[type='tel']");
+  const getNumbersValue = input => input.value.replace(/\D/g, "");
+  const phoneInputHandler = e => {
+    let v = getNumbersValue(e.target);
+
+    if (!v) return (e.target.value = "");
+    if (~["7", "8", "9"].indexOf(v[0])) {
+      switch (v[0]) {
+        case "9": {
+          e.target.value = `+7 ${v}`;
+          break;
+        }
+        case "7": {
+          e.target.value = `+7 ${v}`;
+          break;
+        }
+        case "8": {
+          break;
+        }
+      }
+    }
+  };
+
+  for (const phone of phoneInputs) {
+    phone.addEventListener("input", phoneInputHandler);
+  }
+});
